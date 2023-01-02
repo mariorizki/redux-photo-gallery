364160f65e2e9779d1a96c0fe6f8450c85b11de0
Mario Rizki <mariorizki07@gmail.com>
2023-01-03 01:25:26 +0700

    dispatch getPhotos and get state from redux store using selector

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPhotos } from './features/gallerySlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const photos = useSelector((state) => state.gallery.photos);
+  //gallery from reducer name in store.js and photos come from initial state in reducer
+
+  useEffect(() => {
+    dispatch(getPhotos());
+  }, []);
+
+  console.log(photos);
   return (
     <div className="App">
       <h1>Redux Photo Gallery</h1>
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { galleryReducer } from '../features/gallerySlice';
+import galleryReducer from '../features/gallerySlice';
 
 export const store = configureStore({
   reducer: {
