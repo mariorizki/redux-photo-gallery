8313ad32e96b4018f43fd27254d4722d2601dd4f
Mario Rizki <mariorizki07@gmail.com>
2023-01-03 00:20:07 +0700

    setup redux done

---
+++ src/features/gallerySlice.js
@@ -0,0 +1,29 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getPhotos = createAsyncThunk('photos/getPhotos', async () => {
+  const response = await fetch('https://picsum.photos/v2/list?page=3&limit=9');
+  const formattedResponse = response.json();
+  return formattedResponse;
+});
+
+export const gallerySlice = createSlice({
+  name: 'gallery',
+  initialState: {
+    photos: [],
+    isLoading: false,
+  },
+  extraReducers: {
+    [getPhotos.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getPhotos.fulfilled]: (state, action) => {
+      state.photos = action.payload;
+      state.isLoading = false;
+    },
+    [getPhotos.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  },
+});
+
+export default gallerySlice.reducer;
